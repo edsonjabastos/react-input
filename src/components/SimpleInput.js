529d6f189df9cf42a713cd14291c1aa9fe50cb1d
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-01-14 14:15:35 -0300

    Validation Feedback & Handling the "touched" State

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,25 +1,40 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Name input is valid!");
+    }
+  }, [enteredNameIsValid]);
   const nameInputHandler = (event) => {
     setEnteredName(event.target.value);
   };
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+    setEnteredNameTouched(true);
     if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
       return;
     }
+    setEnteredNameIsValid(true);
     console.log(enteredName);
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
     // nameInputRef.current.value = ""; NOT IDEAL, DON'T MANIPULATE THE DOM
     setEnteredName("");
   };
+  const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
+  const nameInputClasses = nameInputIsValid
+    ? "form-control invalid"
+    : "form-control";
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -28,6 +43,9 @@ const SimpleInput = (props) => {
           onChange={nameInputHandler}
           value={enteredName}
         />
+        {nameInputIsValid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
